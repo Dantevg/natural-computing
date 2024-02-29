2296baa9a8af3c2cfb0b3f5f7c91509898af548a
RedPolygon <dantevg006@gmail.com>
2024-02-29 11:32:26 +0100

    Quickly add cli args and save functionality

--- ca-ui/src/main.rs
+++ ca-ui/src/main.rs
@@ -1,6 +1,12 @@
-use std::time::Instant;
+use std::{
+	fs::{create_dir, File},
+	io::BufWriter,
+	path::PathBuf,
+	time::Instant,
+};
 
 use cellular_automata::world::World;
+use clap::Parser;
 use cpm::{
 	example::{ExampleCPM, ExampleCell},
 	CPM,
@@ -17,36 +23,83 @@ use winit::{
 
 const WIDTH: usize = 200;
 const HEIGHT: usize = 200;
-const SCALE: usize = 3;
+const SCALE: usize = 4;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args {
+	#[arg(short, long, value_name = "FILE")]
+	output: Option<PathBuf>,
+
+	#[arg(long, value_name = "ITERATIONS", default_value_t = 0)]
+	save_interval: u32,
+
+	#[arg(long, default_value_t = 13)]
+	cell_grid: usize,
+
+	#[arg(long, default_value_t = 5)]
+	obstacle_grid: usize,
+
+	#[arg(long, default_value_t = 20.0)]
+	temp: f32,
+
+	#[arg(long, default_value_t = 20.0)]
+	l_adhesion: f32,
+
+	#[arg(long, default_value_t = 200)]
+	volume: u32,
+
+	#[arg(long, default_value_t = 50.0)]
+	l_volume: f32,
+
+	#[arg(long, default_value_t = 180)]
+	perimeter: u32,
+
+	#[arg(long, default_value_t = 2.0)]
+	l_perimeter: f32,
+
+	#[arg(long, default_value_t = 80)]
+	max_act: u8,
+
+	#[arg(long, default_value_t = 300.0)]
+	l_act: f32,
+}
 
 fn main() {
+	let args = Args::parse();
+
 	let mut world: World<WIDTH, HEIGHT, _> = World::default();
-	for x in 0..5 {
-		for y in 0..5 {
-			world.img[(x * WIDTH / 5, y * HEIGHT / 5)] =
-				ExampleCell(x as u8 * 5 + y as u8 + 1, 80, true);
+	for x in 0..args.obstacle_grid {
+		for y in 0..args.obstacle_grid {
+			world.img[(
+				x * WIDTH / args.obstacle_grid,
+				y * HEIGHT / args.obstacle_grid,
+			)] = ExampleCell((x * args.obstacle_grid + y + 1) as u8, 80, true);
 		}
 	}
-	for x in 0..13 {
-		for y in 0..13 {
-			world.img[(x * WIDTH / 13 + 8, y * HEIGHT / 13 + 8)] =
-				ExampleCell(x as u8 * 13 + y as u8 + 1, 80, false);
+	for x in 0..args.cell_grid {
+		for y in 0..args.cell_grid {
+			world.img[(
+				x * WIDTH / args.cell_grid + 8,
+				y * HEIGHT / args.cell_grid + 8,
+			)] = ExampleCell((x * args.cell_grid + y + 1) as u8, 80, false);
 		}
 	}
 	let mut model = ExampleCPM::new(
-		20.0,  // these comments are here to keep the parameters on separate lines
-		20.0,  //
-		200,   //
-		50.0,  //
-		180,   //
-		2.0,   //
-		80,    //
-		300.0, //
+		args.temp,        // these comments are here to keep the parameters on separate lines
+		args.l_adhesion,  //
+		args.volume,      //
+		args.l_volume,    //
+		args.perimeter,   //
+		args.l_perimeter, //
+		args.max_act,     //
+		args.l_act,       //
 		&world,
 	);
 
 	let mut running = true;
-	let mut speed = 16;
+	let mut speed = 1;
+	let mut iter = 0;
 
 	let event_loop = EventLoop::new().unwrap();
 	event_loop.set_control_flow(ControlFlow::Wait);
@@ -81,6 +134,12 @@ fn main() {
 						if running {
 							for _ in 0..speed {
 								model.step(&mut world);
+								iter += 1;
+								if args.output.is_some()
+									&& args.save_interval > 0 && iter % args.save_interval == 0
+								{
+									save(&args, &pixels, iter);
+								}
 							}
 							window.request_redraw();
 						}
@@ -119,6 +178,7 @@ fn main() {
 						{
 							let start_time = Instant::now();
 							model.step(&mut world);
+							iter += 1;
 							let update_time = Instant::now();
 							println!(
 								"update: {:4}us",
@@ -133,4 +193,21 @@ fn main() {
 			}
 		})
 		.unwrap();
+}
+
+fn save(args: &Args, pixels: &Pixels, i: u32) {
+	let mut filename = args.output.as_ref().unwrap().clone();
+	if !filename.is_dir() {
+		create_dir(filename.clone()).unwrap();
+	}
+	filename.push(format!("{i}.png"));
+	let file = File::create(filename).unwrap();
+	let ref mut file_writer = BufWriter::new(file);
+
+	let mut png_encoder =
+		png::Encoder::new(file_writer, (WIDTH * SCALE) as u32, (HEIGHT * SCALE) as u32);
+	png_encoder.set_color(png::ColorType::Rgba);
+	png_encoder.set_depth(png::BitDepth::Eight);
+	let mut png_writer = png_encoder.write_header().unwrap();
+	png_writer.write_image_data(&pixels.frame()).unwrap();
 }
